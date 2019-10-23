9bdbf3fca3185f0dbfa53a2f4b2165804041ea6b
DCCA <dcca12@gmail.com>
2019-10-23 15:58:32 -0700

    *add buttons to carousel *add js count for clicks in add buttons from carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+console.log("Conected");
+// Setting vars
+var buttons = document.querySelectorAll("button");
+var added = "";
+
+for(i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", clickCheck, false);
+}
+
+function clickCheck(){
+    if(this.classList.contains("add-to-cart")){
+        console.log("Clicked")
+    }
+}
+
+
+
+
+// Init carousel
 $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true
   });
+
