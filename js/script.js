d6ff1189725bf403ec5c72a2c4071082bc3bab2e
DCCA <dcca12@gmail.com>
2019-10-26 16:22:35 -0700

    *add smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
-console.log("Conected");
-// Setting vars
-var buttons = document.querySelectorAll("button");
-var added = "";
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll("button");
+    const navLinks = document.querySelectorAll("nav li");
 
-for(i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", clickCheck, false);
-}
+    //Smooth scroll
+    navLinks.forEach(function(element) {
+        // add click listener for the element in this iteration of the loop
+        element.addEventListener("click", function(event){
+            event.preventDefault();
+        // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
+            let href = this.querySelector('a').href;
+        // get the section by its id using the string from the last step
+            let sec = href.split('.html');
+        // scroll to that section with .scrollIntoView()
+            document.querySelector(sec[1]).scrollIntoView({ 
+                behavior: 'smooth' 
+            });
+        });
+      
+    });
 
-function clickCheck(){
-    if(this.classList.contains("add-to-cart")){
-        console.log("Clicked")
-    }
-}
+    // for(i = 0; i < buttons.length; i++){
+    //     buttons[i].addEventListener("click", clickCheck, false);
+    // }
 
+    // function clickCheck(){
+    //     if(this.classList.contains("add-to-cart")){
+    //         console.log("Clicked")
+    //     }
+    // }
 
+    // Init carousel
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
+});
 
-
-// Init carousel
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
-  });
 
