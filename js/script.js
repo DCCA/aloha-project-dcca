968fe21d1887b70ca5d4cf131c545def71ed1a99
DCCA <dcca12@gmail.com>
2019-10-28 20:07:28 -0700

    *change color of footer email *organize js file

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll("nav li");
-    const form = document.querySelector('#form-sign-up');
-    const submitBtn = document.getElementById('submit-btn');
-
-    //Smooth scroll
-    navLinks.forEach(function(element) {
-        // add click listener for the element in this iteration of the loop
-        element.addEventListener("click", function(event){
-            event.preventDefault();
-        // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
-            let href = this.querySelector('a').hash;
-        // scroll to that section with .scrollIntoView()
-            document.querySelector(href).scrollIntoView({ 
-                behavior: 'smooth' 
-            });
-        });
-    });
-
+    const submitBtn = document.getElementById("submit-btn");
     // Init carousel
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
+    $(".main-carousel").flickity({
+        cellAlign: "left",
         contain: true
     });
-
-    submitBtn.addEventListener('click', checkEmail);
-
+    // Add listeners
+    navLinks.forEach(function(element) {
+        element.addEventListener("click", smoothScroll)
+    });
+    submitBtn.addEventListener("click", checkEmail);
+    // Functions
+    function smoothScroll(event){
+        event.preventDefault();
+            let href = this.querySelector("a").hash;
+            document.querySelector(href).scrollIntoView({ 
+                behavior: "smooth" 
+            });
+        };
     function checkEmail(event){
         event.preventDefault();
-        let email = document.getElementById('input-field').value;
+        let email = document.getElementById("input-field").value;
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
             alert("Thanks for subscribing!")
         } else{
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Aloha Website
 ### by Daniel C C Andrade
 
+This is my first responsive website. 
